0a18da4a48050bc0bf3f3fd1cbaf56e7e870c9e7
wowter-code <ionut.horia.as@gmail.com>
2022-11-04 15:50:40 +0200

    Add files via upload

---
+++ kali_tools.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Intall apt https
+sudo apt update
+sudo apt install apt-transport-https
+
+# Add Brave Browser Sources
+echo "Setting up Brave sources"
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# Add Sublime Sources
+echo "Setting up Sublime sources"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# Install Google Chrome
+echo "Install Google Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Remove Java 18
+sudo apt purge -y openjdk-18-jre:amd64 openjdk-18-jre-headless:amd64
+
+# Update and add necessary packages
+echo "Installing Packages and Tools"
+sudo apt update
+sudo apt install -y python3-pip default-jdk brave-browser feroxbuster golang sublime-text neo4j bloodhound
+
+#Fix Go PATH
+echo -e "export PATH=\$PATH:\$HOME/go/bin" >> $HOME/.profile
+source $HOME/.profile
+
+# Install go-tools for recon and vuln scan
+sudo rm /usr/bin/httpx
+echo "Installing go-tools"
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install github.com/sensepost/gowitness@latest
+go install github.com/jaeles-project/gospider@latest
+
+# Install VSCode
+echo "Installing VSCode"
+curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o code.deb
+sudo dpkg -i code.deb
+rm code.deb
+
+# Install BurpPro
+echo "Installing BurpPro"
+curl -L "https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.9.5&type=Linux" -o burp_pro.sh
+bash burp_pro.sh
+rm burp_pro.sh
+
+# Remove BurpCommunity
+sudo apt -y remove burpsuite
+
+# Install Nessus
+echo "Installing Nessus"
+curl --request GET \
+  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.4.1-ubuntu1404_amd64.deb' \
+  --output 'Nessus-10.4.1-ubuntu1404_amd64.deb'
+sudo dpkg -i Nessus-10.4.1-ubuntu1404_amd64.deb
+rm Nessus-10.4.1-ubuntu1404_amd64.deb
